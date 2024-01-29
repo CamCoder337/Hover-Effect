2fd0eed87031ebb3fe6e0036025c1d4d3bbf93ad
CamCoder337 <camcoder337@gmail.com>
2024-01-29 01:29:46 +0100

    Add color property to data objects

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,21 @@ const data = [
       description: "A vast, arid and often inhospitable expanse, the desert is an ecosystem characterized by landscapes of sand, rock and dunes. Often associated with extreme climatic conditions, the desert is a place where life has evolved to adapt to high temperatures and limited water availability. Vast stretches of sand can create spectacular landscapes, and deserts are often marked by austere tranquillity and profound silence.",
       cover_image:"https://images.pexels.com/photos/1001435/pexels-photo-1001435.jpeg",
       images: ["https://images.pexels.com/photos/5598631/pexels-photo-5598631.jpeg", "https://images.pexels.com/photos/9335688/pexels-photo-9335688.jpeg"],
+      color: "bg-yellow-50",
   },
   {
       title: "Jungle",
       description: "The jungle, also known as the rainforest, is a lush, dense expanse of tropical vegetation characterized by an abundance of plants, trees and biodiversity. These warm, humid ecosystems are found mainly near the equator, where the heat and humidity favor lush plant growth. Jungles are home to an incredible diversity of species, from exotic plants to fascinating animals. Their thick canopies create an environment where life thrives in all its forms.",
       cover_image:"https://images.pexels.com/photos/2092828/pexels-photo-2092828.jpeg",
       images: ["https://images.pexels.com/photos/1671324/pexels-photo-1671324.jpeg", "https://images.pexels.com/photos/1481581/pexels-photo-1481581.jpeg"],
+      color: "bg-green-50",
   },
   {
       title: "Mountains",
       description: "Mountains represent imposing geological formations that rise majestically above the earth's surface. These rocky masses, characterized by significant altitudes and steep slopes, offer breathtaking panoramas and diverse ecosystems as altitude changes. Mountains can be covered with lush forests at their feet, alpine meadows higher up, then glaciers and eternal snows on their summits.",
       cover_image:"https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg",
       images: ["https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg", "https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg"],
+      color: "bg-blue-50",
   },
 ]
 
@@ -48,7 +51,7 @@ export default function Home() {
             <div
               key={index}
               className={`border border-slate-300 rounded-lg p-4 shadow-sm cursor-pointer duration-300 
-            ${active === index && show ? "bg-green-50 w-full" : "bg-white w-11/12"}
+            ${active === index && show ? `${item.color} w-full` : "bg-white w-11/12"}
             `}
               onMouseEnter={() => handleShow(index)}
             >
